5ccbb0de12e2c13359fbd8bee08b0ba571b0a1a8
Ronen Lapushner <ronenk17@gmail.com>
2016-06-13 00:43:30 +0300

    Finished MeasureDisplay class. Testing will be done when the fetch and connect logic will be completed.

--- MeasureDisplay.cpp
+++ MeasureDisplay.cpp
@@ -6,9 +6,61 @@ MeasureDisplay::MeasureDisplay(QWidget *parent) :
     ui(new Ui::MeasureDisplay)
 {
     ui->setupUi(this);
+
+    // Set size policies
+    ui->lblMeasurement->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
 }
 
 MeasureDisplay::~MeasureDisplay()
 {
     delete ui;
+}
+
+float MeasureDisplay::temperatureDisplayed() const {
+    return _temp;
+}
+
+float MeasureDisplay::humidityDisplayed() const {
+    return _humid;
+}
+
+QDate MeasureDisplay::dateDisplayed() const {
+    return _measureDate;
+}
+
+void MeasureDisplay::setMeasurementDetails(QDate date, float temperature, char unit,
+                                           float humidity) {
+    // Set all details
+    _temp = temperature;
+    _humid = humidity;
+    _measureDate = date;
+    _measureUnit = unit;
+
+    // Set texts
+    ui->lblDate->setText(_measureDate.toString("dddd, dd MMMM yyyy"));
+    ui->lblMeasurement->setText(MEASUREMENT_DISPLAY_TEMPLATE.arg(_temp,
+                                                                 QChar(unit).toUpper().toLatin1()));
+    ui->lblHumidity->setText(HUMIDITY_DISPLAY_TEMPLATE.arg(_humid));
+    ui->lblFeelsLike->setText(FEELS_LIKE_DISPLAY_TEMPLATE.arg(_temp,
+                                                              QChar(unit).toUpper().toLatin1()));
+}
+
+void MeasureDisplay::resizeEvent(QResizeEvent *event) {
+    Q_UNUSED(event);
+
+    // Get font and font matrics
+    QFontMetrics metrics(ui->lblMeasurement->font());
+    QFont labelFont = ui->lblMeasurement->font();
+
+    // Calculate widths
+    int stringWidth = metrics.width(ui->lblMeasurement->text());
+
+    // Find out how much can the font grow
+    float widthRatio = (float)ui->lblMeasurement->width() / (float)stringWidth;
+    int newFontSize = (int)
+            ((labelFont.pixelSize() == -1 ? labelFont.pointSize() : labelFont.pixelSize())
+                                           * widthRatio);
+
+    labelFont.setPointSize(std::min(newFontSize, ui->lblMeasurement->height()));
+    ui->lblMeasurement->setFont(labelFont);
 }
--- MeasureDisplay.h
+++ MeasureDisplay.h
@@ -2,6 +2,8 @@
 #define MEASUREDISPLAY_H
 
 #include <QWidget>
+#include <QDebug>
+#include <QDate>
 
 namespace Ui {
 class MeasureDisplay;
@@ -15,8 +17,28 @@ public:
     explicit MeasureDisplay(QWidget *parent = 0);
     ~MeasureDisplay();
 
+    // Getters for the measurement details
+    float temperatureDisplayed(void) const;
+    float humidityDisplayed(void) const;
+    QDate dateDisplayed(void) const;
+
+public slots:
+    // Setters for all properties
+    void setMeasurementDetails(QDate date, float temperature, char unit, float humidity);
+
+protected:
+    void resizeEvent(QResizeEvent *event);
+
 private:
     Ui::MeasureDisplay *ui;
+    float _temp, _humid;
+    QDate _measureDate;
+    char _measureUnit;
+
+    // Constants
+    const QString MEASUREMENT_DISPLAY_TEMPLATE = tr("%1 °%2");
+    const QString HUMIDITY_DISPLAY_TEMPLATE = tr("Humidity: %1");
+    const QString FEELS_LIKE_DISPLAY_TEMPLATE = tr("(Feels like %1 °%2)");
 };
 
 #endif // MEASUREDISPLAY_H
